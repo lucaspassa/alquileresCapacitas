28b3f076733a1aee6e4020c8a5796c3ed3dc96d7
gauchoaleman <gauchoaleman@yahoo.com>
2016-10-11 21:44:52 -0300

    actualizo disponibilidades

--- recursos.php
+++ recursos.php
@@ -11,26 +11,7 @@
 
 <?php include "include/inc_menu.php"; ?>
 <form>
-<div class="container">
-    <div class="col-sm-6" style="height:130px;">
-        <div class="form-group">
-            <div class='input-group date' id='datetimepicker11'>
-                <input type='text' class="form-control" />
-                <span class="input-group-addon">
-                    <span class="glyphicon glyphicon-calendar">
-                    </span>
-                </span>
-            </div>
-        </div>
-    </div>
-    <script type="text/javascript">
-        $(function () {
-            $('#datetimepicker11').datetimepicker({
-                daysOfWeekDisabled: [0, 6]
-            });
-        });
-    </script>
-</div>
+
 </form>
 
       <!-- Site footer -->
