1e2bb1060a67c03ea27bc9176c7324a0d2d7cb8f
gauchoaleman <gauchoaleman@yahoo.com>
2016-10-20 21:27:12 -0300

    Subo queries de creación y insert

--- sql/Create Database and Tables.sql
+++ sql/Create Database and Tables.sql
@@ -2,8 +2,7 @@ delimiter $$
 
 CREATE DATABASE `capacitas` /*!40100 DEFAULT CHARACTER SET latin1 */$$
 
-
-
+USE capacitas;
 
 CREATE  TABLE `capacitas`.`Cliente` (
   `idCliente` INT NOT NULL AUTO_INCREMENT ,
@@ -24,8 +23,8 @@ CREATE TABLE `alquiler` (
   `idalquiler` int(11) NOT NULL AUTO_INCREMENT,
   `idcliente` int(11) NOT NULL,
   `idrecursoalquiler` int(11) NOT NULL,
-  `fechaInicio` date DEFAULT NULL,
-  `fechaFin` date DEFAULT NULL,
+  `fechaInicio` datetime DEFAULT NULL,
+  `fechaFin` datetime DEFAULT NULL,
   `observaciones` varchar(256) DEFAULT NULL,
   `precioTotal` float DEFAULT NULL,
   `pagado` int(11) DEFAULT NULL,
@@ -35,21 +34,6 @@ CREATE TABLE `alquiler` (
 
 delimiter $$
 
-CREATE TABLE `cliente` (
-  `idCliente` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) NOT NULL,
-  `apellido` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `dni` varchar(9) NOT NULL,
-  `sexo` varchar(10) NOT NULL,
-  `numeroLicencia` varchar(45) DEFAULT NULL,
-  `vencimientoLicencia` date DEFAULT NULL,
-  `fechaNacimiento` date NOT NULL,
-  `telefono` varchar(45) NOT NULL,
-  PRIMARY KEY (`idCliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
-
-delimiter $$
 
 CREATE TABLE `recurso` (
   `idrecurso` int(11) NOT NULL AUTO_INCREMENT,
--- sql/Insert Data.sql
+++ sql/Insert Data.sql
@@ -1,3 +1,4 @@
+-- NombreTabla: alquiler
 INSERT INTO `capacitas`.`alquiler` (`idcliente`, `idrecursoalquiler`, `fechaInicio`, `fechaFin`, `observaciones`, `precioTotal`) 
 VALUES ('1', '1', '15/10/2016 15:00', '17/10/2016 15:00', 'idcliente 1, idrecurso 1', '200');
 INSERT INTO `capacitas`.`alquiler` (`idcliente`, `idrecursoalquiler`, `fechaInicio`, `fechaFin`, `observaciones`, `precioTotal`) 
@@ -5,16 +6,22 @@ VALUES ('2', '2', '18/10/2016 13;00', '20/10/2016 15;00', 'idcliente 2, idrecurs
 INSERT INTO `capacitas`.`alquiler` (`idcliente`, `idrecursoalquiler`, `fechaInicio`, `fechaFin`, `observaciones`, `precioTotal`) 
 VALUES ('2', '1', '18/10/2016 19;00', '20/10/2016 22;00', 'idcliente 2, idrecurso 1', '850');
 
-INSERT INTO `capacitas`.`cliente` (`nombre`, `apellido`, `email`, `dni`, `sexo`, `numeroLicencia`, `fechaNacimiento`, `telefono`) 
-VALUES ('Stefan', 'Ronacher', 'gauchoaleman@yahoo.com', '92844240', 'M', '', '19/05/1978', '154301742');
-INSERT INTO `capacitas`.`cliente` (`nombre`, `apellido`, `email`, `dni`, `sexo`, `numeroLicencia`, `vencimientoLicencia`, `fechaNacimiento`, `telefono`) 
-VALUES ('Lucas', 'Passalaqua', 'lucas@yahoo.com', '22433544', 'M', '8912378912', '129831298', '7/6/82', '129838389');
-INSERT INTO `capacitas`.`cliente` (`nombre`, `apellido`, `email`, `dni`, `sexo`, `numeroLicencia`, `vencimientoLicencia`, `fechaNacimiento`, `telefono`) 
-VALUES ('Federico', 'Bordini', 'fede@yahoo.com', '82989988', 'M', '839283389', '923829899', '9/8/90', '183934839');
 
+
+-- NombreTabla: cliente
+INSERT INTO `capacitas`.`cliente` (`nombre`, `apellido`, `email`, `dni`, `sexo`, `numeroLicencia`, `fechaNacimiento`, `telefono`) 
+VALUES ('Stefan', 'Ronacher', 'gauchoaleman@yahoo.com', '92844240', 'M', '', '1978-05-19', '154301742');
+INSERT INTO `capacitas`.`cliente` (`nombre`, `apellido`, `email`, `dni`, `sexo`, `numeroLicencia`, `vencimientoLicencia`, `fechaNacimiento`, `telefono`) 
+VALUES ('Lucas', 'Passalaqua', 'lucas@yahoo.com', '22433544', 'M', '8912378912', '2017-06-03', '1982-06-07', '129838389');
+INSERT INTO `capacitas`.`cliente` (`nombre`, `apellido`, `email`, `dni`, `sexo`, `numeroLicencia`, `vencimientoLicencia`, `fechaNacimiento`, `telefono`) 
+VALUES ('Federico', 'Bordini', 'fede@yahoo.com', '82989988', 'M', '839283389', '2019-06-03', '1995-20-10', '183934839');
+
+-- NombreTabla: recurso
 INSERT INTO `capacitas`.`recurso` (`idrecurso`, `tipoRecurso`, `descripcion`, `ocupado`, `costoxhora`) 
 VALUES ('', '1', 'Idrecurso 1, tipo 1', '1', '75');
 INSERT INTO `capacitas`.`recurso` (`idrecurso`, `tipoRecurso`, `descripcion`, `ocupado`, `costoxhora`) 
 VALUES ('', '2', 'Idrecurso 2, tipo 2', '0', '100');
 INSERT INTO `capacitas`.`recurso` (`tipoRecurso`, `descripcion`, `ocupado`, `costoxhora`) 
 VALUES ('3', 'idrecurso 3, tipo 3', '1', '75');
+
+
